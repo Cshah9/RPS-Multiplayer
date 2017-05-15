e49a2a7667a6491a3f0dd9711b67c58a6d3fb805
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-05-15 02:53:51 -0400

    added player sign in to jumbotron, and sign in on click to remove inputs and display name on local webpage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 var debugMode = true;
 
+
+/*debug - console logs up to 2 strings if debugMode flag is set to true */
 function debug(description, info) {
 
 	if(debugMode) {
@@ -11,8 +13,47 @@ function debug(description, info) {
 
 debug("game.js");
 
-$(".shape-choice").click(function(){
-	debug("on click", ".shape-choice");
-	//alert("clicked");
+var playerName = "";
+var playerId = 0;
+
+
+
+$(".shape-choice").click(choiceMade);
+
+$("#sign-in").click(function(){
+	event.preventDefault();
+
+	debug("#sign-in on-click");
+	playerName = $("#name").val().trim();
+	debug(playerName);
+	$("#sign-in-alert").toggleClass("alert-warning alert-success");
+	$(".sign-in").remove();
+	$("#sign-in-row").append("<p>Hi <span class='label label-success'>" + playerName + "</span> ! You are player " + playerId + ".</p>");
+	
+
 })
+
+function choiceMade(){
+	debug("choiceMade()");
+	//alert("clicked");
+	
+}
+
+/* document.ready will initalize the game once the DOM is loaded */
+$(document).ready(function() {
+	
+	debug("document.ready()")
+
+//	initalizeGame();
+
+});
+
+function initalizeGame() {
+
+	debug("initalizeGame()");
+
+	//hide game choices until user logs in
+	$("#player-1-choices").toggle();
+	$("#player-2-choices").toggle();
+}
 
